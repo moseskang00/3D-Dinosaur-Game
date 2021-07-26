ee2ba7c7d20ed430db73c599655a3d2a5ebb76a4
aawells2 <aawells2@asu.edu>
2021-07-25 20:33:38 -0700

    physics

---
+++ Project/Assets/Scripts/pixelRock.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pixelRock : MonoBehaviour
+{
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+        void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag == "Enemy")
+            {
+                audioSource.Play();
+            }
+        }
+    
+}
